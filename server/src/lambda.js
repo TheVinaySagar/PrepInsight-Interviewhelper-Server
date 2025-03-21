4afdae3e805d98440cc3db938d45cd3723b802df
Vinay Sagar <vinaysagar4445@gmail.com>
2025-03-21 17:48:17 +0530

    some done2

--- server/src/lambda.js
+++ server/src/lambda.js
@@ -8,7 +8,7 @@ import awsServerlessExpress from 'aws-serverless-express';
 import { auth } from './config/firebase.js';
 import authRoutes from './routes/authRoutes.js';
 import interviewRoutes from './routes/interviewsRoutes.js';
-import userRoutes from './routes/usersRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import chatRoutes from './routes/chatRoutes.js';
 import commentsRoutes from './routes/commentsRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
