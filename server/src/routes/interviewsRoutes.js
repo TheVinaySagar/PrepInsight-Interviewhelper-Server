5a044945945d44073613d2e484eeb532343d33a3
Vinay Sagar <vinaysagar4445@gmail.com>
2025-03-31 21:50:48 +0530

    Some cache

--- server/src/routes/interviewsRoutes.js
+++ server/src/routes/interviewsRoutes.js
@@ -3,11 +3,22 @@ import InterviewsController from "../controllers/InterviewsController.js";
 
 const InterviewsRoutes = Router();
 
+InterviewsRoutes.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  next();
+});
 InterviewsRoutes.get('/', InterviewsController.getInterviews);
 InterviewsRoutes.get('/search', InterviewsController.searchInterviews);
 InterviewsRoutes.get('/user-interviews', InterviewsController.userInterviews);
-InterviewsRoutes.get('/trending', InterviewsController.getTrendingInterviews);
-InterviewsRoutes.get('/:id', InterviewsController.interviewById);
+InterviewsRoutes.get('/trending', (req, res, next) => {
+  res.setHeader('Cache-Control', 'public, max-age=600');
+  next();
+}, InterviewsController.getTrendingInterviews);
+InterviewsRoutes.get('/:id', (req, res, next) => {
+  res.setHeader('Cache-Control', 'public, max-age=300');
+  next();
+}, InterviewsController.interviewById);
+
 InterviewsRoutes.post('/', InterviewsController.createInterview);
 InterviewsRoutes.post('/:id/like', InterviewsController.likeInterview);
 InterviewsRoutes.put('/:id', InterviewsController.updateInterview);
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,11 +3,23 @@ import auth from "../middleware/firebase.js";
 import UserController from "../controllers/userController.js";
 const userRoutes = Router();
 
+userRoutes.use((req, res, next) => {
+  // Apply no-cache headers to sensitive data routes
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  next();
+});
+
 userRoutes.get('/profile', auth, UserController.profile)
 userRoutes.get('/interviews', auth, UserController.interviews)
 userRoutes.put('/profile', auth, UserController.profile_update)
 userRoutes.get('/profile-data', auth, UserController.profile_data)
 userRoutes.get('/stats', auth, UserController.stats)
 userRoutes.get("/photos", UserController.latestUserPhotos);
+
+
+userRoutes.get("/photos", (req, res, next) => {
+  res.setHeader('Cache-Control', 'public, max-age=86400');
+  next();
+}, UserController.latestUserPhotos);
 
 export default userRoutes;
