11ac4a7799d254ecd042263fb67a022759fcf47b
Vinay Sagar <vinaysagar4445@gmail.com>
2025-03-31 21:22:12 +0530

    User f

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -8,5 +8,6 @@ userRoutes.get('/interviews', auth, UserController.interviews)
 userRoutes.put('/profile', auth, UserController.profile_update)
 userRoutes.get('/profile-data', auth, UserController.profile_data)
 userRoutes.get('/stats', auth, UserController.stats)
+userRoutes.get("/photos", UserController.latestUserPhotos);
 
 export default userRoutes;
